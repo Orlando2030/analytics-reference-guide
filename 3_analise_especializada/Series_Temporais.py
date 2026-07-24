d1db0c69e9e6bac2492c87700193f712b53aa459
Orlando Santos de Oliveira <orlando2030@gmail.com.br>
2026-07-23 23:15:34 -0300

    Revisão 01

---
+++ 3_analise_especializada/Series_Temporais.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def mean_absolute_error(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred))
+
+def root_mean_squared_error(y_true, y_pred):
+    return np.sqrt(np.mean((y_true - y_pred) ** 2))
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    return np.mean(np.abs((y_true - y_pred) / y_true))
